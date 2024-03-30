14a5d130c10e38a3c34e70a180c35cc3476f0ea4
AjeetKumarYadav07 <iamsupertrader07@gmail.com>
2024-03-30 22:48:07 +0530

    add some changes

--- part3/script.js
+++ part3/script.js
@@ -5,7 +5,7 @@ gsap.to("#page1 h1 " , {
     scrollTrigger:{
         trigger:"#page1",
         scroller:"body",
-        markers:true,
+        // markers:true,
         start:"top 0",
         scrub:3,
         pin:true,
@@ -13,3 +13,64 @@ gsap.to("#page1 h1 " , {
 
     }
 })
+
+
+var tl = gsap.timeline()
+
+// function time() {
+//     var a = 0;
+
+//      setInterval(function() {
+//        
+//              a = a + Math.floor (Math.random()*10)
+        //   if (a<100) {
+//             document.querySelector("#timer").
+//             innerHTML = a +"%"
+//         } else {
+//            a =100
+//            document.querySelector("#timer ").
+//            innerHTML = a+ "%"
+//         }
+//     }, 100);
+// }
+
+// time();
+
+
+function time () {
+
+   var a = 0;
+     setInterval (function () {
+        if (a<100) {
+              a++
+               document.querySelector("#timer").
+               innerHTML = a+ "%"
+        } else {
+            a = 100
+            document.querySelector("#timer").
+            innerHTML = a+"%"
+        }
+     }, 20)
+
+}
+
+
+tl.to("#loader h1 ", {
+     delay:0.5,
+     duration:1,
+     onStart:time()
+})
+
+tl.to("#loader " , {
+    top:" -100vh",
+    delay:1,
+    duration:2.5
+})
+
+
+
+  document.addEventListener("click" , neFuntion);
+
+   function neFuntion () {
+    document.querySelector("#letter ").innerHTML = "Cute girl"
+   }
